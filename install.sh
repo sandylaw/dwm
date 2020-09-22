cdd880b725e92f1bb648a20740fc14cdfb721331
sandylaw <freelxs@gmail.com>
2020-09-22 09:18:41 +0800

    add dmw source

--- install.sh
+++ install.sh
@@ -1,74 +1,79 @@
 #!/usr/bin/bash
 #v1.0 by sandylaw <freelxs@gmail.com> 2020-09-20
+# shellcheck disable=SC1091
 source '/etc/os-release'
-ID=$(echo ${ID}|tr [A-Z] [a-z])
+ID=$(echo "${ID}" | tr '[:upper:]]' '[:lower:]')
 VERSION=$(echo "${VERSION}" | awk -F "[()]" '{print $2}')
 check_system() {
     if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]]; then
-        echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font}"
-		sudo yum update
+        echo -e "当前系统为 Centos ${VERSION_ID} ${VERSION}"
+        echo "Next time!"
+        exit 1
+        sudo yum update
         INS="yum install"
     elif [[ "${ID}" == "debian" ]] || [[ "${ID}" == "ubuntu" ]] || [[ "${ID}" == "deepin" ]] || [[ "${ID}" == "uos" ]]; then
-        echo -e "${OK} ${GreenBG} 当前系统为 Debian系"
-		sudo apt update
+        echo -e "当前系统为 Debian系"
+        sudo apt update
         INS="apt install"
-		sudo $INS libx11-dev libxrandr-dev libxft-dev libxinerama-dev libjpeg-dev -y
+        sudo "$INS" libx11-dev libxrandr-dev libxft-dev libxinerama-dev libjpeg-dev firefox-esr -y
     elif [[ "${ID}" == "archlinux"  ]] || [[ "${ID}" == "manjaro"  ]] || [[ "${ID}" == "endeavouros"  ]]; then
-        echo -e "${OK} ${GreenBG} 当前系统为 $ID"
-		sudo pacman -Sy
+        echo -e "当前系统为 $ID"
+        sudo pacman -Sy
         INS="pacman -S --noconfirm"
-		sudo $INS --noconfirm base-devel
+        sudo "$INS" --noconfirm base-devel firefox
     else
-        echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font}"
+        echo -e "当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断"
         exit 1
     fi
 }
 
 function install_dwm() {
-	if ! [ -f dwm.diff ]; then
-		wget -N --no-check-certificate -q -O dwm.diff "https://raw.githubusercontent.com/sandylaw/dwm/master/dwm.diff"
-	fi
-	if ! [ -f st.diff ]; then
-		wget -N --no-check-certificate -q -O st.diff "https://raw.githubusercontent.com/sandylaw/dwm/master/st.diff"
-	fi
-	if ! [ -f bg.jpg ]; then
-		wget -N --no-check-certificate -q -O bg.jpg "https://raw.githubusercontent.com/sandylaw/dwm/master/bg.jpg"
-	fi
-    yes | sudo $INS recordmydesktop git firefox firefox-esr feh compton xautolock scrot pcmanfm
+    rm -rf st dmenu dwm slstatus farbfeld sent slock || true
+    yes | sudo "$INS" recordmydesktop git feh compton xautolock scrot pcmanfm || exit
+    git clone git@github.com:sandylaw/dwm.git .
+    # if ! [ -f dwm.diff ]; then
+    # 	wget -N --no-check-certificate -q -O dwm.diff "https://raw.githubusercontent.com/sandylaw/dwm/master/dwm.diff"
+    # fi
+    # if ! [ -f st.diff ]; then
+    # 	wget -N --no-check-certificate -q -O st.diff "https://raw.githubusercontent.com/sandylaw/dwm/master/st.diff"
+    # fi
+    # if ! [ -f bg.jpg ]; then
+    # 	wget -N --no-check-certificate -q -O bg.jpg "https://raw.githubusercontent.com/sandylaw/dwm/master/bg.jpg"
+    # fi
     TUSER="$USER"
-	git clone https://github.com/juliusHuelsmann/st.git
-	cd st||exit
-	git checkout master
-	cp ../st.diff .
-	patch -p1 < st.diff
-	# Optional: Use my xresources 
-	xrdb -merge .Xresources
+    # git clone https://github.com/juliusHuelsmann/st.git
+    cd st || exit
+    git checkout master
+    cp ../st.diff .
+    patch -p1 < st.diff
+    # Optional: Use my xresources
+    xrdb -merge .Xresources
     cd ..
-    git clone https://git.suckless.org/dwm/
+    # git clone https://git.suckless.org/dwm/
     cp dwm.diff dwm/
-    cd dwm
-	sed -ri "s/_USERNAME/$TUSER/g" dwm.diff
+    cd dwm || exit
+    sed -ri "s/_USERNAME/$TUSER/g" dwm.diff
     patch -p1 < dwm.diff
     cd ..
-    git clone https://git.suckless.org/dmenu/
-    git clone https://git.suckless.org/slock
+    # git clone https://git.suckless.org/dmenu/
+    # git clone https://git.suckless.org/slock
     wget -O - https://dl.suckless.org/farbfeld/farbfeld-4.tar.gz | tar -xz
     mv farbfeld* farbfeld
-    git clone git://git.suckless.org/slstatus
+    # git clone git://git.suckless.org/slstatus
     mkdir -p sent/
     pushd sent/ > /dev/null || exit
     wget -O - https://dl.suckless.org/tools/sent-1.tar.gz | tar -xz
     popd > /dev/null || exit
-	mkdir -p /home/"$TUSER"/screenshots > /dev/null
+    mkdir -p /home/"$TUSER"/screenshots > /dev/null
     tree -d .
     DWM=(st dmenu dwm slstatus farbfeld sent slock)
     for x in ${DWM[*]}; do
         pushd "$x" > /dev/null || exit
-		rm config.h
+        rm config.h
         sudo make clean install
         popd > /dev/null || exit
     done
-    rm -rf st dmenu dwm slstatus farbfeld sent slock
+
     cat << EOF | sudo tee /usr/share/xsessions/dwm.desktop
 [Desktop Entry]
 Name=dwm
